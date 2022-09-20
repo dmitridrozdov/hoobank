00698ee22a5446c5928a3ca13f6f1809c816f897
dmitridrozdov <dm.drozdov@gmail.com>
2022-09-20 15:20:56 +1000

    rest sections

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,27 @@ const App = () => (
       </div>
     </div>
     
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        {/* <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer /> */}
+        Stats
+        Business
+        Billing
+        CardDeal
+        Testimonials
+        Clients
+        CTA
+        Footer
+      </div>
+    </div>
+
   </div>
   )
 
